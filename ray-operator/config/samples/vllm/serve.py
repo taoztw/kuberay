3a3e2bb72fc418c5a39c642aced47701b7c1de35
tzone <35052758+taoztw@users.noreply.github.com>
2025-02-10 18:24:34 +0800

    Update serve.py
    trust-remote-code
    
    Signed-off-by: tzone <35052758+taoztw@users.noreply.github.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -132,4 +132,4 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
 
 
 model = build_app(
-    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
+    {"model": os.environ['MODEL_ID'], "trust-remote-code": True, "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
